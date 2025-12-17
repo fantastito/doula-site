6a4b40595cdbf82c3ab52b7d46f18ebb1b0bb45b
Simon Budden <simon.budden@gmail.com>
2025-12-17 13:46:15 +0000

    Added image handling and management

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Threading.Tasks;
 using Statiq.App;
 using Statiq.Web;
+using Statiq.Images;
 
 namespace MySite
 {
@@ -10,6 +11,18 @@ namespace MySite
       await Bootstrapper
         .Factory
         .CreateWeb(args)
+                .BuildPipeline("ResizeImages", builder =>
+            {
+                builder.WithInputReadFiles("assets/images/*.{jpg,png,gif}");
+                builder.WithInputModules(new MutateImage()
+                    .Resize(1920, null).OutputAsWebp().And()
+                    .Resize(1200, null).OutputAsWebp().And()
+                    .Resize(900, null).OutputAsWebp().And()
+                    .Resize(640, null).OutputAsWebp().And()
+                    .Resize(1920, null).OutputAsJpeg()
+                );
+                builder.WithOutputWriteFiles();
+            })
         .RunAsync();
   }
 }
